29e104554e8fe214e0eeaa22e6c9fb6ef16a0035
hemang05 <joshihemang05@gmail.com>
2020-05-24 17:32:30 +0530

    my first commit

---
+++ src/test1.py
@@ -0,0 +1,4 @@
+def git_learn():
+	print('start your git learning hemang')
+	
+git_learn()
---
+++ src/test2.js
@@ -0,0 +1,9 @@
+function learn(){
+	
+	a="can i learn git"
+	if(a==="can i learn git"){
+		console.log("you atlist try so you can");
+	}
+	else{
+		alert("you never tried");
+	}
